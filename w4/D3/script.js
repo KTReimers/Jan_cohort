8f589ae6af5ca3d75762351fd273938c49b132df
Kyle <reimers95@gmail.com>
2023-02-02 20:37:14 -0600

    Algo session

--- w4/D3/script.js
+++ w4/D3/script.js
@@ -1,5 +1,44 @@
 // 1) Given two integers a and b, which can be positive or negative, find the sum of all the integers between and including them and return it. If the two numbers are equal return a or b.
 
+function sum(a,b){    //define function
+    var sum =0;       // set sum variable equal 0 (placeholder)
+    if(a==b){         // checking if a == b
+        return a;       // give us back  one of the numbers
+    }else if (a<b){      //checking if a is smaller than b
+        for(var i=a;i<=b;i++){    // loop or count from a to b
+            sum +=i;     // take the value of sum and increase it by the iterator
+        }
+        return sum     // return out of the function with the sum
+    }else{      //only option left is that a>b
+        for(var i=b;i<=a;i++){     // looping from b to a
+            sum +=i;     // take the value of sum and increase it by the iterator
+        }
+        return sum    //return the sum
+    }
+}
+var result =sum(100, 40)
+console.log(result);
+
+var tagName = document.querySelector('#idNameOfTag')
+tagName.innerText = result
+
+
+function sum2(a,b){    //define function
+    var sum =0;     // set sum variable equal 0 (placeholder)
+    if (a<b){   //checking if a is smaller than b
+        for(var i=a;i<=b;i++){ // loop or count from a to b
+            sum +=i; // take the value of sum and increase it by the iterator
+        }
+        return sum // return out of the function with the sum
+    }else{ //only option left is that a>b
+        for(var i=b;i<=a;i++){ // looping from b to a
+            sum +=i; // take the value of sum and increase it by the iterator
+        }
+        return sum //return the sum
+    }
+}
+var result =sum2(100, 100)
+console.log(result);
 
 
 
